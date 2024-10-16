0ed95289d227f8cee18f42685047120d0d18381b
ahmed22hany <ahaany5673@gmail.com>
2024-10-16 19:03:54 +0300

    adding layout and some styles making

--- src/components/shared/TopBar.jsx
+++ src/components/shared/TopBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Topbar() {
   return (
     <nav className="topbar">
-      <Link to="/" className="flex items-center gap-4">
+      <Link to="/home" className="flex items-center gap-4">
         <img src="/assets/logo.svg" alt="logo" width={28} height={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">
           TrendWave
